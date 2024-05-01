3d5994811f642e35586ad4d4d1905ffbf0ae4a80
Derek Ho <tmptsm@protonmail.com>
2024-05-01 12:54:30 +0800

    added theme styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,56 @@
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: [
+        "Inter",
+        "system-ui",
+        "Avenir",
+        "Helvetica",
+        "Arial",
+        "sans-serif",
+      ],
+    },
+    extend: {
+      colors: {
+        primary: colors.blue,
+        secondary: colors.yellow,
+      },
+      animation: {
+        blink: "blink 1s steps(1, end) infinite",
+        "move-up": "move-up 0.7s ease-out 1",
+      },
+      keyframes: {
+        blink: {
+          "0%": {
+            opacity: 1,
+          },
+          "50%": {
+            opacity: 0,
+          },
+          "100%": {
+            opacity: 1,
+          },
+        },
+        "move-up": {
+          "0%": {
+            opacity: 0,
+            transform: "translateY(+5rem)",
+          },
+          "90%": {
+            opacity: 1,
+            transform: "translateY(-1rem)",
+          },
+          "100%": {
+            opacity: 1,
+            transform: "translateY(+0rem)",
+          },
+        },
+      },
+    },
   },
   plugins: [],
 };
