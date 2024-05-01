4dd75c4d1a3927ca2fda48cc479618651c1d2b72
Derek Ho <tmptsm@protonmail.com>
2024-05-01 11:19:47 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Fragment } from "react";
+import "./index.css";
+
+function App() {
+  return (
+    <Fragment>
+      <h1>React Timezones</h1>
+    </Fragment>
+  );
+}
+
+export default App;
