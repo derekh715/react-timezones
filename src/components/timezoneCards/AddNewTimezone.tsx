9f430b15999a9c3e5e2428daef7a80e00e4c1904
Derek Ho <tmptsm@protonmail.com>
2024-05-02 13:44:46 +0800

    added feather icons for react

--- src/components/timezoneCards/AddNewTimezone.tsx
+++ src/components/timezoneCards/AddNewTimezone.tsx
@@ -1,7 +1,12 @@
+import { Plus } from "react-feather";
+
 function AddNewTimezone() {
   return (
     <div className="bg-none text-gray-800 rounded-lg border-gray-200 border-4 border-dashed p-8 animate-move-up cursor-pointer transition-all hover:-translate-y-4 hover:shadow-lg">
-      <span className="text-lg font-semibold">Add New Timezone</span>
+      <span className="text-lg font-semibold inline-flex items-center border-gray-800 border-b-2 border-solid">
+        <span>Add New Timezone</span>
+        <Plus className="ml-1 inline-block" />
+      </span>
     </div>
   );
 }
--- src/components/timezoneCards/LargeDisplay.tsx
+++ src/components/timezoneCards/LargeDisplay.tsx
@@ -1,3 +1,4 @@
+import { ArrowRight } from "react-feather";
 import TimeDisplay from "./CurrentTime";
 
 function LargeDisplay() {
@@ -5,8 +6,9 @@ function LargeDisplay() {
     <div className="min-h-64 rounded-lg text-white p-20 my-12 animate-move-up bg-gradient-to-br from-primary-600 to-primary-900">
       <h2 className="text-5xl font-bold mb-12">Country/City</h2>
       <TimeDisplay className="text-4xl font-extrabold mb-8" />
-      <button className="border-none outline-none bg-none text-white underline cursor-pointer text-xl hover:text-gray-200 transition-colors">
+      <button className="outline-none bg-none border-b-2 border-white hover:border-gray-200 border-solid text-white  cursor-pointer text-xl hover:text-gray-200 transition-colors">
         <span>View In Map</span>
+        <ArrowRight className="inline-block ml-1" />
       </button>
     </div>
   );
