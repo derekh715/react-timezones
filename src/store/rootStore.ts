6beb73f26b7761be385d9618940431e5566b4fce
Derek Ho <tmptsm@protonmail.com>
2024-05-07 20:18:12 +0800

    added zod for form validation

--- src/store/rootStore.ts
+++ src/store/rootStore.ts
@@ -1,27 +1,36 @@
 import { LatLng, LatLngTuple, LeafletMouseEvent } from "leaflet";
+import { typeToFlattenedError, z } from "zod";
 import { create } from "zustand";
 import { immer } from "zustand/middleware/immer";
 import { GeoapifyTimezone } from "../types";
 import { getTimezoneByLatlng } from "../utils/geoapify";
+
+const formSchema = z.object({
+  lat: z.number().gte(-90).lte(90),
+  lng: z.number().gte(-180).lte(180),
+  country: z.string().min(1),
+  city: z.string().min(1),
+});
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+type PartialBy<T, K extends keyof T> = Omit<T, K> & Partial<Pick<T, K>>;
+
+export type FormValues = z.infer<typeof formSchema>;
+export type OptionalFormValues = PartialBy<FormValues, "lat" | "lng">;
+
+export type FormNames = keyof FormValues;
 
 export interface RootState {
   modalVisible: boolean;
   openModal: () => void;
   dismissModal: () => void;
   chooseLocation: (event: LeafletMouseEvent) => void;
-  form: FormValues;
+  form: OptionalFormValues;
   timezoneInfo?: GeoapifyTimezone;
+  fieldErrors: typeToFlattenedError<FormValues>["fieldErrors"];
   updateForm: (name: keyof FormValues, value: string) => void;
+  submit: () => void;
 }
-
-export interface FormValues {
-  lat?: number;
-  lng?: number;
-  country: string;
-  city: string;
-}
-
-export type FormNames = keyof FormValues;
 
 export const defaultCenter: LatLngTuple = [24.02, 121.38];
 
@@ -86,6 +95,18 @@ export const useRootStore = create<RootState>()(
           state.form[name] = value;
         });
       }
+    },
+    fieldErrors: {},
+    submit: () => {
+      set((state) => {
+        const result = formSchema.safeParse(state.form);
+        if (!result.success) {
+          state.fieldErrors = result.error.flatten().fieldErrors;
+          return;
+        }
+        // add new result
+        state.fieldErrors = {};
+      });
     },
   }))
 );
