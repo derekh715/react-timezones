69435f2e0b5117bfb26187df5169c2f10cc17ba2
Derek Ho <tmptsm@protonmail.com>
2024-06-12 11:44:41 +0800

    static copy leaflet icons and auto deployment (instead of gh-pages)

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,20 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: "node_modules/leaflet/dist/images/*",
+          dest: "",
+        },
+      ],
+    }),
+  ],
   // moment library needs this
   // https://stackoverflow.com/questions/76395714/react-moment-library-import-causes-error-cannot-set-properties-of-undefined-s
   resolve: {
